26fcba69c2a1bc7eb20fd12daee8d623cb109b2c
Eugenio Tampieri <eugenio@eutampieri.eu>
2021-08-19 21:58:52 +0200

    Added time utilities

---
+++ portable_time.c
@@ -0,0 +1,15 @@
+#include "portable_time.h"
+
+#ifdef __unix__
+#include <time.h>
+uint64_t get_current_time_ms()
+{
+    return (uint64_t)(time(NULL));
+}
+#endif
+#ifdef __AVR__
+uint64_t get_current_time_ms()
+{
+    return (uint64_t)(millis() / 1000);
+}
+#endif
---
+++ portable_time.h
@@ -0,0 +1,3 @@
+#include <inttypes.h>
+
+uint64_t get_current_time_ms();
